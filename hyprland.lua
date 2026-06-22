4fd113eabacf6d0ab571b69da7ffc34a0cc03ba4
Victor Filizola <victorblizzard@hotmail.com>
2026-06-22 20:26:01 -0300

    initial commit of my hyperland configs

---
+++ hyprland.lua
@@ -0,0 +1,12 @@
+-- Hyprland Lua config
+-- https://wiki.hypr.land/Configuring/Start/
+
+require("env")
+require("monitors")
+require("input")
+require("appearance")
+require("animations")
+require("programs")
+require("keybinds")
+require("startup")
+require("windowrules")
